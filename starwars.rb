82304e7b3b0344a34340f608e056af64ba77c17a
hukl <contact@smyck.org>
2010-06-24 09:32:11 +0200

    added timing for starwars

--- starwars.rb
+++ starwars.rb
@@ -9,22 +9,24 @@ File.open(ARGV[0], "r") do |file|
     frame << line.ljust(100).gsub(/\n/, "")[7..62]
 
     if (file.lineno - 1) % 14 == 0
-      frames << frame.join
+      line.match(/^\d+/)[0].to_i.times do
+        frames << frame.join
+      end
       frame = []
     end
   end
 end
 
-Display.connect("127.0.0.1", 2342) do
+Display.connect("172.23.42.120", 2342) do
   @framerate = 0
   frames.each do |frame|
 
     start_time = Time.now
     update (frame + @framerate.to_i.to_s.ljust(56))
-    
+
     sleep(0.04)
     end_time = Time.now
-    
+
     @framerate = 1 / (end_time - start_time)
   end
 end
